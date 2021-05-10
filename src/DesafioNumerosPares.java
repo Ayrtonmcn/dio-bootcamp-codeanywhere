5f450dfd1dedde0adc98ec963d1e97bb2280ebb4
Ayrtonmcn <ayrtonmcn@gmail.com>
2021-05-10 14:37:36 -0300

    Resolução alternando os números 6 e 13

--- src/DesafioNumerosPares.java
+++ src/DesafioNumerosPares.java
@@ -1,2 +1,21 @@
 public class DesafioNumerosPares {
+    public static void main(String[] args) {
+
+        int valor = 18;
+        int resultado = valor % 2;
+
+        if (resultado == 0) {
+            while (resultado == 0 || resultado < valor) {
+                resultado = resultado + 2;
+                System.out.println(resultado);
+            }
+        }else{
+            resultado = resultado - 1;
+            valor = valor - 1;
+            while (resultado == 0 || resultado < valor){
+                resultado = resultado + 2;
+                System.out.println(resultado);
+            }
+        }
+    }
 }
