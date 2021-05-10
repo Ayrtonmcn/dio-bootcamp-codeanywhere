9a6d0e306c9f42934567a5fb5de0844224ef60d5
Ayrtonmcn <ayrtonmcn@gmail.com>
2021-05-10 15:31:33 -0300

    Código que passou nos testes

--- src/DesafioNumerosPares.java
+++ src/DesafioNumerosPares.java
@@ -1,30 +1,23 @@
+//Crie um programa que leia um número e mostre os números pares até esse número, inclusive ele mesmo.
+
 import java.util.Scanner;
 
 public class DesafioNumerosPares {
     public static void main(String[] args) {
-        // Utilizado para pedir uma entrada
         Scanner entrada = new Scanner(System.in);
-
         int valor;
-        // Será solicitado ao usuário um dado para a variável valor
-        System.out.print("Informe um número: ");
         valor = entrada.nextInt();
-
-        // Fórmula para saber se o número digitado é par ou ímpar
         int resultado = valor % 2;
 
-        // Se o resto for 0 o número será par
         if (resultado == 0) {
             while (resultado == 0 || resultado < valor) {
                 resultado = resultado + 2;
                 System.out.println(resultado);
             }
-
-        // Se o resultado for 1 o número será ímpar
-        }else{
+        } else {
             resultado = resultado - 1;
             valor = valor - 1;
-            while (resultado == 0 || resultado < valor){
+            while (resultado == 0 || resultado < valor) {
                 resultado = resultado + 2;
                 System.out.println(resultado);
             }
